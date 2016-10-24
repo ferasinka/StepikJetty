d3e8c3f0b6483076b383b946f64199932c5b37ea
ferasinka <iam@ferasinka.com>
2016-10-24 22:15:34 +0300

    Added dependencies and Frontend.java

---
+++ src/main/java/com/ferasinka/stepikjetty/Main.java
@@ -0,0 +1,22 @@
+package com.ferasinka.stepikjetty;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Frontend frontend = new Frontend();
+
+		Server server = new Server(8080);
+
+		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+
+		server.setHandler(context);
+
+		context.addServlet(new ServletHolder(frontend), "/authform");
+
+		server.start();
+		server.join();
+	}
+}
