b1d05883be22ad55e1ec11bac04ba579779ac91f
ferasinka <iam@ferasinka.com>
2016-10-25 20:49:54 +0300

    L1.1 Initial state

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,20 +1,19 @@
-package com.ferasinka.stepikjetty;
+package main;
 
+import servlets.AllRequestsServlet;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		Frontend frontend = new Frontend();
-
-		Server server = new Server(8080);
+		AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
 
 		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+		context.addServlet(new ServletHolder(allRequestsServlet), "/*");
 
+		Server server = new Server(8080);
 		server.setHandler(context);
-
-		context.addServlet(new ServletHolder(frontend), "/authform");
 
 		server.start();
 		server.join();
